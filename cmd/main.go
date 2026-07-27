ca1844496c909ee5bb8967bb737c9577305b7591
Nikolai Luzhkov <luzhkovn@icloud.com>
2026-07-27 13:35:12 +0300

    Реализовано финальное задание 6 спринта

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"log"
+	"os"
+
+	"github.com/luzhkovn/go1fl-sprint6-final-tpl/internal/server"
+)
+
 func main() {
 
+	logger := log.New(os.Stdout, "INFO: ", log.LstdFlags)
+	srv := server.NewServer(logger)
+	err := srv.Start()
+	if err != nil {
+		logger.Fatal(err)
+	}
 }
